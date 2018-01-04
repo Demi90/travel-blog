496ac85462fd14a488c7745483a1abadff405bcc
Diem Le <diem@pivot.jp>
2018-01-04 13:41:04 +0700

    add custom theme

---
+++ wp-content/themes/mytravel/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>HSD ヨシダグループ│歯科医院の内装・改装・建築</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="HSD ヨシダグループ│歯科医院の内装・改装・建築">
+	<meta name="keywords" content="関東,東京,歯科,内装,開業,テナント,デザイン,設計,吉田製作所,ヒール・システム・デザイン">
+	<meta property="og:title" content="HSD ヨシダグループ│歯科医院の内装・改装・建築" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://hsd.jp" />
+	<meta property="og:image" content="http://hsd.jp/asset/img/ogp.png" />
+	<meta property="og:site_name" content="Heal System Design" />
+	<meta property="og:description" content="HSD ヨシダグループ│歯科医院の内装・改装・建築" />
+
+	<link rel="apple-touch-icon" href="/asset/img/favicon.ico" />
+	<link rel="shortcut icon" href="/asset/img/favicon.ico" />
+
+	<link href="/asset/css/style.css" rel="stylesheet" />
+
+</head>
+<body class="topBody animation-in">
+
+<div class="bannerContainer">
+	<div id="top-slides" class="top-slides">
+		<div class="slides-container">
+			<img src="/asset/img/top/1.png" alt="">
+			<img src="/asset/img/top/3.png" alt="">
+			<img src="/asset/img/top/4.png" alt="">
+			<img src="/asset/img/top/2.png" alt="">
+		</div>
+	</div>
+</div>
+
+<button class="c-hamburger c-hamburger--htx only-sm">
+	<span>toggle menu</span>
+</button>
+<div id="pageMenu" class="pageMenu">
+	<ul class="pageMenu__list">
+		<li><a class="pageMenu__list__item pageMenu__list__item--current" href="/"><span>HOME</span></a></li>
+		<li><a class="pageMenu__list__item" href="/concept"><span>CONCEPT</span></a></li>
+		<li><a class="pageMenu__list__item" href="/about"><span>ABOUT</span></a></li>
+		<li><a class="pageMenu__list__item" href="/work"><span>WORK</span></a></li>
+	</ul>
+</div>
+
+<div class="mainContainer mainContainer--top">
+	<div class="pageHeader">
+		<!-- <a href="/"><img class="header__hsdLogo" src="/asset/img/logo-hsd-scan.png" alt="Heal system Design"></a> -->
+		<a class="header__yoshidaLogo__link" href="http://www.yoshida-net.co.jp"><img src="/asset/img/logo-yoshida.png" alt="yoshida"></a>
+	</div>
+
+	<div class="contentSection">
+		<img class="hsdTopLogo" src="/asset/img/logo-hsd-scan-large.png" alt="Heal system Design">
+	</div>
+
+	<div class="pageFooter pageFooter--top">
+		<p class="pageFooter_lead">一級建築士事務所　東京都知事登録　第51644号　建設業許可<span class="only-lg">　</span><br class="only-sm">東京都知事　許可(般-20）第121293号</p>
+		<p class="pageFooter_copyright">Copyright(C)2017 THE YOSHIDA DENTAL MFG.CO.,LTD All rights reserved.</p>
+	</div>
+</div>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-36038448-1', 'auto');
+    ga('send', 'pageview');
+</script>
+<script src="//code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="/asset/js/jquery.animate-enhanced.min.js"></script>
+<script src="/asset/js/jquery.superslides.min.js"></script>
+<script src="/asset/js/common.min.js"></script>
+
+</body>
+</html>
